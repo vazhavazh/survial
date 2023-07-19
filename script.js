bdfa2ae8bcfcd634649597e35ac1f6b1cf631a79
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-07-19 13:12:55 +0300

    player drawwed

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const canvas = document.querySelector('canvas');
-const context = document.getContext('2d');
-const width = document.documentElement.clientWidth;
-const height = document.documentElement.clientHeight;
+import { Player } from "./player.js";
+
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+canvas.width = document.documentElement.clientWidth;
+canvas.height = document.documentElement.clientHeight;
 
 let player = new Player(canvas.width / 2, canvas.height / 2, context);
+
+
+function animate() {
+	requestAnimationFrame(animate);
+	player.drawImg();
+}
+
+
+animate();
